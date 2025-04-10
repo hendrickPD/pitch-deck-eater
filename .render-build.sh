464ed978db874dbdc749a8c6f56f16c0d54b74ee
Your Name <hendrick@palmdrive.vc>
2025-04-10 11:36:49 -0400

    Copy Chrome setup from docsend-slack-bot

---
+++ .render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Install dependencies
+npm install 
